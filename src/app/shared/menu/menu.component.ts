8dd494fae13f7eec59878054871cc6377ee231a5
juansanchez-conectarse <juan.sanchez@conectarse.com.co>
2022-06-05 11:46:50 -0500

    Desaparecer menu al hacer scroll

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  opacity: number = 1;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+  navbarScroll() {
+    let top = window.scrollY;
+    this.opacity = (top<100)?1:0;
+}
 
 }
