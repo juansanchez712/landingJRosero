45cd382b43825ace4ffdb1ed134f31cf3c1a8c69
juansanchez-conectarse <juan.sanchez@conectarse.com.co>
2022-07-17 12:43:50 -0500

    Version desktop con correciones

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,17 +7,28 @@ import { Component, HostListener, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
+  @Input() flag: boolean = false;
   opacity: number = 1;
 
-  constructor() { }
+  lista = document.getElementsByClassName('optionMenu');
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    if(this.router.url == '/work'){
+      this.lista[0].setAttribute("style", "color: black")
+      this.lista[1].setAttribute("style", "color: black")
+      this.lista[2].setAttribute("style", "color: black")
+    }
   }
 
 
   navbarScroll() {
     let top = window.scrollY;
     this.opacity = (top<10)?1:0;
+  }
+
+  goToHome(){
+    this.router.navigate(['']);
   }
 
 }
