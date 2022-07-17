f65b58dbb1559cf9d279aa41cc1e2c0b9332d299
juansanchez-conectarse <juan.sanchez@conectarse.com.co>
2022-07-17 18:01:23 -0500

    Version a revision

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './feature/home/home.component';
+import { SliderComponent } from './shared/slider/slider.component';
 import { WorkComponent } from './shared/work/work.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'work', component: WorkComponent}
+  { path: 'work', component: WorkComponent},
+  { path: 'slider', component: SliderComponent}
 ];
 
 @NgModule({
