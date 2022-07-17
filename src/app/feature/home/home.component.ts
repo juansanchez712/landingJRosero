9034f850bb39a657639d25f12943f6fe6b7a95cf
juansanchez-conectarse <juan.sanchez@conectarse.com.co>
2022-07-17 07:52:14 -0500

    Mobile version2 borrador

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -8,33 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   cards = [
     {
-      image: "../../../assets/5C3 1.png",
-      category: "First category",
-      description: "Lorem ipsum dolor sit amet",
+      image: "../../../assets/5C3 1-1.png",
+      description: "Exterior spaces",
       alt: "Desk with yellow chair"
     },
     {
-      image: "../../../assets/5C4 1.png",
-      category: "Second category",
-      description: "Lorem ipsum dolor sit amet",
+      image: "../../../assets/5C3 1.png",
+      description: "Research into the latest trends in workspaces that help design, manufacture solutions and create inspiring spaces.",
       alt: "Desk with brown background"
     },
     {
-      image: "../../../assets/5C4 1.1.png",
-      category: "Third category",
-      description: "Lorem ipsum dolor sit amet",
-      alt: "Desk with red chairs" 
-    },
-    {
       image: "../../../assets/5C3 1.1.png",
-      category: "Fourth category",
-      description: "Lorem ipsum dolor sit amet",
-      alt: "Beer monkey" 
-    },
-    {
-      image: "../../../assets/5C4 1.1.png",
-      category: "Third category",
-      description: "Lorem ipsum dolor sit amet",
+      description: "Textures and colors",
       alt: "Desk with red chairs" 
     }
   ];
